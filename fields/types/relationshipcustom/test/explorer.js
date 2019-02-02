c039b54f2bbeb938a081bbe60bd9e76ee677b9ff
Trần Đức Tiến <ductienas@gmail.com>
2019-02-02 18:06:55 +0700

    relationshipcustom explorer

--- fields/types/relationshipcustom/test/explorer.js
+++ fields/types/relationshipcustom/test/explorer.js
@@ -1,11 +1,11 @@
 module.exports = {
-	Field: require('../RelationshipField'),
-	Filter: require('../RelationshipFilter'),
-	readme: require('fs').readFileSync('./fields/types/relationship/Readme.md', 'utf8'),
+	Field: require('../RelationshipCustomField'),
+	Filter: require('../RelationshipCustomFilter'),
+	readme: require('fs').readFileSync('./fields/types/relationshipcustom/Readme.md', 'utf8'),
 	section: 'Miscellaneous',
 	spec: [{
-		label: 'Single Relationship',
-		path: 'relationship',
+		label: 'Single Relationship Custom',
+		path: 'relationshipcustom',
 		// createInline isn't available in the explorer because it depends on
 		// real list definitions and the FieldTypes bundle
 		// createInline: true,
@@ -17,8 +17,8 @@ module.exports = {
 		},
 		value: '',
 	}, {
-		label: 'Many Relationship',
-		path: 'manyrelationship',
+		label: 'Many Relationship Custom',
+		path: 'manyrelationshipcustom',
 		many: true,
 		// createInline: true,
 		refList: {
